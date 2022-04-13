865525fe0da0d1ac8df5b7144dc76b642102d9d8
marcio.george <marcio.george@phoebustecnologia.com.br>
2022-04-13 09:06:38 -0300

    Implementação da listagem das categorias

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../model/category.model';
+import { CategoryService } from '../service/category.service';
+import toastr from "toastr";
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(
+    private service: CategoryService
+    ) { }
 
   ngOnInit(): void {
+    this.getCategories();    
   }
 
+  getCategories(): void {
+    this.service.getAll().subscribe(response => {
+      this.categories = response;
+    }, (error) => {
+      toastr.error("Erro ao listar categorias")
+    }); 
+  }
+
+  deleteCategory(category) {
+    const confirmDelete = confirm('Deseja realmente excluir este item?');
+    if(confirmDelete) {
+      this.service.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => toastr.error("Erro ao tentar excluir!")
+      );
+    }
+  }
 }
